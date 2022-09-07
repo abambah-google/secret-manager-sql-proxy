05d080b4c3bcb71d0c66a6629d0ddda2f8eb6fe7
mgaur10 <46466824+mgaur10@users.noreply.github.com>
2022-09-07 12:13:35 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -15,28 +15,31 @@
  */
 
 
-
 output "_01_host_network_project_id" {
   value = google_project.demo_project.project_id
 }
 
-output "_02_retrieve_db_password" {
-  value = "gcloud secrets versions access ${google_secret_manager_secret_version.sql_db_user_password.id} --secret ${google_secret_manager_secret.sql_db_user_password.id}"
-}
-
-output "_03_start_ssh_tunnel" {
+output "_02_start_ssh_tunnel" {
   value = "gcloud compute ssh ${google_compute_instance.proxy_server.name} --project ${var.demo_project_id}${random_string.id.result} --zone ${var.network_zone} --tunnel-through-iap"
 }
 
-output "_04_sql_instance_connection_name" {
+output "_03_sql_instance_connection_name" {
   value = google_sql_database_instance.private_sql_instance.connection_name
 }
 
-output "_05_initiate_sql_listner_connection" {
+output "_04_initiate_sql_listner_connection" {
   value = "cloud_sql_proxy -instances=${var.demo_project_id}${random_string.id.result}:${var.network_region}:sql-instance=tcp:0.0.0.0:5432"
 }
-
-output "_06_sql_client_command" {
+   
+  output "_05_retrieve_db_username" {
+  value = "gcloud secrets versions access ${google_secret_manager_secret_version.sql_db_user_name.id} --secret ${google_secret_manager_secret.sql_db_user_name.id}"
+}
+   
+ output "_06_retrieve_db_password" {
+  value = "gcloud secrets versions access ${google_secret_manager_secret_version.sql_db_user_password.id} --secret ${google_secret_manager_secret.sql_db_user_password.id}"
+}
+   
+output "_07_sql_client_command" {
   value = "psql \"host=127.0.0.1 port=5432 sslmode=disable dbname=${google_sql_database.records_db.name} user=${google_sql_user.user_dev_access.name}\""
-  sensitive = true
+ 
 }
